9ce03c4330c785b43f72f1290aabff50ef7ed02b
Akeel <agilowen@gmail.com>
2011-04-30 03:12:00 -0400

    setup main page with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Wisbit::Application.routes.draw do
+  get "main/index"
+
+  resources :categories
+
   resources :quotes
 
   # The priority is based upon order of creation:
@@ -51,7 +55,7 @@ Wisbit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+	root :to => "main#index" 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
