a9590a04abf0ecce9e157c8b7db9b03cca910b97
Akeel <agilowen@gmail.com>
2011-05-08 03:32:49 -0400

    script used to generate seeds.rb

---
+++ db/seed.rb
@@ -0,0 +1,24 @@
+f=File.open("seeds.rb","w")
+
+f.puts "Quote.delete_all\n"
+f.puts "Category.delete_all\n"
+
+category=1
+
+Dir.glob("*.txt") do |file|
+	
+	filename=file.gsub(/.txt/,'')
+
+	f.puts "Category.create(:name=>\"#{filename}\")"
+
+	File.open(file,"r").each do |line|
+		line=line.gsub(/\"/,'\\\"')
+		line=line.gsub(/\n/,'')
+		f.puts "Quote.create(:quote=>\"#{line}\", :score=> 0, :category_id=>#{category}, :matchups => 0)" if line!="" and line!=" "
+	end
+
+	category+=1
+
+end
+
+f.close
